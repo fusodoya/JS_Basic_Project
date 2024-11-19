3b6f495b9960f24536e54fa8c018c67015dfa181
fusodoya <fusodoya@gmail.com>
2024-11-19 23:56:37 +0700

    first project, backgound color flipper

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["red", "orange", "yellow", "green", "cyan", "blue", "purple", "white", "black", "gray"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+    const randomNUmber = getRandomNumber();
+    console.log(randomNUmber);
+
+    document.body.style.backgroundColor = colors[randomNUmber];
+    color.textContent = colors[randomNUmber];
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
